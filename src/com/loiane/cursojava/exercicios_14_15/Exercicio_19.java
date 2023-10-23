4e9f4413bd21a64fff74a07d2206f529ea934a53
Bruno Carvalho Work <brunocarvalho.devsenai@gmail.com>
2023-10-23 01:15:27 -0300

    atualização Ex19

--- src/com/loiane/cursojava/exercicios_14_15/Exercicio_19.java
+++ src/com/loiane/cursojava/exercicios_14_15/Exercicio_19.java
@@ -1,12 +1,53 @@
 package com.loiane.cursojava.exercicios_14_15;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio_19 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int cent = 0, dez = 0, unid = 0;
+		int input;
+		boolean validate = true, validate2 = true;
+		String inputStr;
+		
+		do {
+			
+			inputStr = JOptionPane.showInputDialog("Digite um número menor que 1000");
+			input = Integer.parseInt(inputStr);
+			
+			if ((input > 1000) || (input<0)) {
+				do {
+					inputStr = JOptionPane.showInputDialog(null, "Valor digitado inválido\n\n "
+							+ "Deseja digitar novamente?");
+					if(inputStr.equalsIgnoreCase("y")) {
+						continue;						
+					}
+					if(inputStr.equalsIgnoreCase("n")) {
+						validate = false;
+						validate2 = false;
+						break;
+					}
+				}while(validate2);
+			}else if((input > 0) || (input <=1000)) {
+				validate = false;
+			}
+			
+		}while(validate);
+		
+		for (; input >= 100; cent++) {
+			input = input - 100;
+		}
+		for (; input >= 10; dez++){
+			input = input - 10;
+		}
+		unid = input;
+		JOptionPane.showMessageDialog(null, "Tabela Unidade\n\n"
+				+ "O valor de  "+inputStr+" tem:\n"
+				+ "\nCentenas = "+cent
+				+ "\nDezanas = "+dez
+				+ "\nUnidades = "+unid);
 	}
-
 }
 //19.Faça um Programa que leia um número inteiro menor que 1000 e 
 //imprima a quantidade de centenas, dezenas e unidades do mesmo.
