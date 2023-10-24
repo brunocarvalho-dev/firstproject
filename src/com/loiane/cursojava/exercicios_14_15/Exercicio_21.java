45730ab223a3cf004ec8800031fc4bf9c3a5d182
User <User@DESKTOP-KSS5GQ6>
2023-10-24 04:58:02 -0300

    Atualização Ex21

--- src/com/loiane/cursojava/exercicios_14_15/Exercicio_21.java
+++ src/com/loiane/cursojava/exercicios_14_15/Exercicio_21.java
@@ -1,10 +1,57 @@
 package com.loiane.cursojava.exercicios_14_15;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio_21 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String conta = "";
+		
+		int saque = 0;
+		int nota100 = 0, nota50 = 0, nota10 = 0, nota5 = 0, nota1 = 0;
+		
+		do{
+			conta = JOptionPane.showInputDialog("Digite o valor para sacar\n\n"
+												+ "O caixa tem notas de de:\n 1, 5,"
+												+ "10, 50 e 100 reais");
+			saque = Integer.parseInt(conta);
+			if (!(saque > 0)) {
+				JOptionPane.showMessageDialog(null, "conta invalido\n Digite novamente");
+			}
+		
+		}while(saque <= 0);
+		
+		
+		for(;saque >= 100;saque -= 100) {
+			nota100 ++;
+		}
 
+		for(;saque >= 50;saque -= 50) {
+			nota50 ++;
+		}
+
+		for(;saque >= 10;saque -= 10) {
+			nota10 ++;
+		}
+
+		for(;saque >= 5;saque -= 5) {
+			nota5 ++;
+		}
+
+		for(;saque >= 1;saque -= 1) {
+			nota1 ++;
+		}
+		
+		JOptionPane.showMessageDialog(null, "Saque Realizado com Sucesso!\n\n"
+				+ "Valor sacador : R$"+conta+",00"
+				+ "\n\nNotas fornecidas:\n"
+				+ nota100 + " - R$ 100,00\n"
+				+ nota50 + " - R$ 500,00\n"
+				+ nota10 + " - R$ 10,00\n"
+				+ nota5 + " - R$ 5,00\n"
+				+ nota1 + " - R$ R1,00\n");
+	
 	}
 
 }
