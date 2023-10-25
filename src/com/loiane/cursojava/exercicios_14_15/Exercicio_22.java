88a81db407aa9c7cb1048d6a61fd024ffa3d54b2
Bruno carvalho <141652001+brunocarvalho-dev@users.noreply.github.com>
2023-10-24 23:26:12 -0300

    Update Exercicio_22.java
    Atualização Ex22

--- src/com/loiane/cursojava/exercicios_14_15/Exercicio_22.java
+++ src/com/loiane/cursojava/exercicios_14_15/Exercicio_22.java
@@ -1,10 +1,21 @@
 package com.loiane.cursojava.exercicios_14_15;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio_22 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int imparPar = 0;
+		
+		String input = JOptionPane.showInputDialog("Digite um número para saber sé e´impar ou par:");
+		
+		imparPar = Integer.parseInt(input);
+		
+		if(imparPar % 2 == 0) {
+			JOptionPane.showMessageDialog(null, "O numero "+imparPar+" é PAR");
+		}else {
+			JOptionPane.showMessageDialog(null, "O numero "+imparPar+" é IMPAR");
+		}
 	}
 
 }
