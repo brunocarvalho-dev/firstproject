15121345326d1d2ca2b28b2dc499f5d0112bbbfa
Bruno carvalho <141652001+brunocarvalho-dev@users.noreply.github.com>
2023-11-07 05:56:37 -0300

    Update Exercicio_23.java
    Atualização Ex23

--- src/com/loiane/cursojava/exercicios_14_15/Exercicio_23.java
+++ src/com/loiane/cursojava/exercicios_14_15/Exercicio_23.java
@@ -1,10 +1,25 @@
 package com.loiane.cursojava.exercicios_14_15;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio_23 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double doubOrInt = 0;
+		int intDouble = 0;
+		
+		String input = JOptionPane.showInputDialog("Digite um número para saber sé é inteiro ou decimal:");
+		
+		doubOrInt = Double.parseDouble(input.replace(",", "."));
+		intDouble = (int) doubOrInt;
+		double compare = Double.valueOf(intDouble);
+		
+		if (doubOrInt > compare) {
+			JOptionPane.showMessageDialog(null, "O número "+ input + " é um decimal");
+		}else {
+			
+			JOptionPane.showMessageDialog(null, "O número "+ input + " é um inteiro");
+		}
 	}
 
 }
