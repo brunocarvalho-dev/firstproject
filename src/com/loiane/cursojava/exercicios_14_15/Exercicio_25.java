d5740042291773d95ff544ddcaa19269ab86e53f
Bruno Carvalho <brunocarvalho.devsenai@gmail.com>
2024-01-12 06:30:23 -0300

    Atualização exercicio 25

--- src/com/loiane/cursojava/exercicios_14_15/Exercicio_25.java
+++ src/com/loiane/cursojava/exercicios_14_15/Exercicio_25.java
@@ -1,9 +1,80 @@
 package com.loiane.cursojava.exercicios_14_15;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio_25 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		char [] asks = new char [5];
+		String resposta1;
+		char resp;
+		 
+		resposta1 = JOptionPane.showInputDialog(null, "Telefonou para a vítima?\n 'S' ou 'N'");
+		resp = Character.toUpperCase(resposta1.charAt(0));
+		asks[0] = resp;
+		
+		resposta1 = JOptionPane.showInputDialog(null, "Esteve no local do crime?\n 'S' ou 'N'");
+		resp = Character.toUpperCase(resposta1.charAt(0));
+		asks[1] = resp;
+		
+		resposta1 = JOptionPane.showInputDialog(null, "Mora perto da vítima?\n 'S' ou 'N'"); 
+		resp = Character.toUpperCase(resposta1.charAt(0));
+		asks[2] = resp;
+		
+		resposta1 = JOptionPane.showInputDialog(null, "Devia para a vítima?\n 'S' ou 'N'");
+		resp = Character.toUpperCase(resposta1.charAt(0));
+		asks[3] = resp;
+		
+		resposta1 = JOptionPane.showInputDialog(null, "Já trabalhou com a vítima?\n 'S' ou 'N'"); 
+		resp = Character.toUpperCase(resposta1.charAt(0));
+		asks[4] = resp;
+		
+		interrogatorioPolicial(asks);
+		
+
+	}
+	public static void interrogatorioPolicial(char [] quest)
+	{
+		char [] asks = quest; 
+		int respostas = 0;
+		String veredito;
+		for(char ask: asks)
+		{
+			if('S' == (ask))
+			{
+				respostas++;
+			}
+			else continue;
+		}
+		switch (respostas) 
+		{
+			case 2: {
+				veredito = "Suspeito!";
+				JOptionPane.showMessageDialog(null, "De acondo com a investigação\nO seu veredito foi ...");
+				break;
+	
+			}
+			case 3: {
+			}
+			
+			case 4: {
+				veredito = "Cumplice!";
+				JOptionPane.showMessageDialog(null, "De acondo com a investigação\nO seu veredito foi ...");
+				break;
+			}
+			
+			case 5: {
+				veredito = "Assassino!";
+				JOptionPane.showMessageDialog(null, "De acondo com a investigação\nO seu veredito foi ...");
+				break;
+			}
+			
+			default:
+				veredito = "Inocente!";
+				JOptionPane.showMessageDialog(null, "De acondo com a investigação\nO seu veredito foi ...");
+				break;
+		}
+		JOptionPane.showMessageDialog(null, veredito);
 
 	}
 
--- src/com/loiane/cursojava/exercicios_14_15/Exercicio_28.java
+++ src/com/loiane/cursojava/exercicios_14_15/Exercicio_28.java
@@ -3,7 +3,7 @@ package com.loiane.cursojava.exercicios_14_15;
 public class Exercicio_28 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
