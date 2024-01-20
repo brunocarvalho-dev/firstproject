046b7c85bcb86b19daec5d2526395ca1648e7fcb
Bruno Carvalho <brunocarvalho.devsenai@gmail.com>
2024-01-20 06:02:30 -0300

    Atualização exercicio 26

--- src/com/loiane/cursojava/exercicios_14_15/Exercicio_26.java
+++ src/com/loiane/cursojava/exercicios_14_15/Exercicio_26.java
@@ -1,10 +1,103 @@
 package com.loiane.cursojava.exercicios_14_15;
 
+import java.io.StringWriter;
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
+
 public class Exercicio_26 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		boolean validation = true;
+		double litros = 0;
+		String combustivel = "";
+		do 
+		{
+			String litrosStr = JOptionPane.showInputDialog("Quantos litros deseja abastecer?", "Combustivel em Litros");
+			litros = Double.parseDouble(litrosStr.replaceAll(",", "."));                                                                
+			
+			if(litros > 0)
+			{
+				do
+				{
+					combustivel = (JOptionPane.showInputDialog("Digite o tipo de combustivel: G - Gasolina A - álcool" ));
+					combustivel = String.valueOf(combustivel.charAt(0));
+					if(combustivel.equalsIgnoreCase("G") || combustivel.equalsIgnoreCase("A"))
+					{
+						validation = false;
+					}else
+					{
+						JOptionPane.showMessageDialog(null, "Valor inválido!"); 
+					}
+				}while(validation);
+			}
+			else if(litros == 0)  
+			{
+				JOptionPane.showMessageDialog(null, "Valor inválido!\nA quantidade de Litros não pode ser 0");
+			}
+			else  
+			{
+				JOptionPane.showMessageDialog(null, "Valor inválido!");
+			}
+		}while(validation);
+		
+		valorTotal(litros, combustivel);
+
+	}
+	public static void valorTotal(double litros, String combustivelTipo)
+	{
+		String padrao = "###,###.00";
+		DecimalFormat df = new DecimalFormat(padrao);
+		
+		double combustivelValor = 0, desconto= 0;
+		double combustivelValorGasolina = 2.50;
+		double combustivelValorAlcool = 1.90;
+		
+		String gasOuAlcool = combustivelTipo, totalPagar;
+
+		if(combustivelTipo.equalsIgnoreCase("A"))
+		{
+			gasOuAlcool = "Álcool";
+			combustivelValor = combustivelValorAlcool;
+			if(litros > 20)
+			{
+				desconto = 5;
+			}
+			else
+			{
+				desconto = 3;
+			}
+		}
+		else if(combustivelTipo.equalsIgnoreCase("G"))
+		{
+			gasOuAlcool = "Gasolina";
+			combustivelValor = combustivelValorGasolina;
+			if(litros > 20)
+			{
+				desconto = 6;
+			}
+			else
+			{
+				desconto = 4;
+			}
+		}
+
+		double subtotal = litros * combustivelValor;
+		double valorFinal = (litros * combustivelValor)*((100-desconto)/100);
+		
+		totalPagar = "Nota de Abastecimento\n"
+					+"\nCombustivel: " + gasOuAlcool
+					+"\nValor do combustível: R$ " + df.format(combustivelValor)
+					+"\n\nTotal Abastecido: " + litros + " litros"
+					+"\nSubtotal: R$ " + df.format(subtotal)
+					+"\nDesconto: R$ -" + df.format(subtotal - (subtotal * (100-desconto)/100))
+					+"\n\nTotal: R$ " + df.format(valorFinal);
+		
+		
+		JOptionPane.showMessageDialog(null, "Bem Vindo Ao Posto Tabajara!");
+		totalPagar = String.valueOf(totalPagar);
+		JOptionPane.showMessageDialog(null, totalPagar);
+		
 	}
 
 }
--- src/com/loiane/cursojava/exercicios_14_15/Exercicio_27.java
+++ src/com/loiane/cursojava/exercicios_14_15/Exercicio_27.java
@@ -1,12 +1,59 @@
 package com.loiane.cursojava.exercicios_14_15;
 
+import java.text.DecimalFormat;
+
+import javax.swing.JOptionPane;
+
 public class Exercicio_27 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	public static void frutas(String fruta1, String fruta2, double pesoMaca, double pesoMorango) {
+		
+		String padraoDecimal = "###.###,00";
+		DecimalFormat df = new DecimalFormat(padraoDecimal);
+		
+		double macaPreco = 0, morangoPreco = 0, subtotal = 0, total = 0, desconto = 0;
+		
 
+		if((pesoMaca + pesoMorango) > 8)
+		{
+			macaPreco = 1.50;
+			morangoPreco = 2.20;
+			subtotal = ((pesoMaca * macaPreco) + (pesoMorango * morangoPreco));
+			
+			if(subtotal > 25)
+			{
+				desconto = subtotal * 0.10;
+				total = subtotal - desconto;
+			}
+			
+			else
+			{
+				total = subtotal - desconto;
+			}
+		}
+		
+		else
+		{
+			macaPreco = 1.80;
+			morangoPreco = 2.50;
+			total = subtotal - desconto;
+		}
+		
+		total = "Nota de Compra\n"
+				+"\nFruta: " + fruta1
+				+"\nValor da " + frunta1 + ":  R$ " + df.format(combustivelValor)
+				+"\n\nTotal Abastecido: " + litros + " litros"
+				+"\nSubtotal: R$ " + df.format(subtotal)
+				+"\nDesconto: R$ -" + df.format(subtotal - (subtotal * (100-desconto)/100))
+				+"\n\nTotal: R$ " + df.format(valorFinal);
+	
+		
+
+	}
 }
 //27.Uma fruteira está vendendo frutas com a seguinte tabela de preços:
 //Até 5 Kg Acima de 5 Kg 
